3d5679768cc286db7a8db57706f6ce2c19de1297
Khalid <khalidhasanx3@gmail.com>
2023-08-01 12:09:30 +0530

    LinkedList

--- DSA/LinkedList/DoublyLL.java
+++ DSA/LinkedList/DoublyLL.java
@@ -69,15 +69,48 @@ public void reverse(){
     }
     head = prev;
 }
+public void addLast(int data){
+    Node newNode = new Node(data);
+     if(head==null){
+        head=tail=newNode;
+        return;
+    }
+    tail.next=newNode;
+    tail = newNode;
+    size++;
+}
+public int removeLast(){
+    if(size==0){
+        System.out.println("LL is empty");
+        return Integer.MIN_VALUE;
+    }else if(size==1){
+        int val = head.data;
+        head=tail=null;
+        size =0;
+        return val;
+    }
+    Node prev = head;
+    for (int i = 0; i < size-1; i++) {
+        prev=prev.next;
+        
+    }
+    int val = prev.next.data;
+    prev.next = null;
+    tail = prev;
+    size--;
+    return val;
+}
+
 public static void main(String[] args) {
     DoublyLL dll = new DoublyLL();
+    dll.addLast(4);
     dll.addFirst(3);
     dll.addFirst(2);
     dll.addFirst(1);
     dll.print();
     // System.out.println(dll.size);
     // dll.removeFirst();
-    dll.reverse();
+    dll.removeLast();
     dll.print();
     // System.out.println(dll.size);
 
